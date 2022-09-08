d729d48f98c0336844261d809942ff33e0bbd74b
seanjungmsba <seanjeong1996@gmail.com>
2022-09-08 19:00:25 -0400

    endpoint protected using JWT-encoded bearer token support

---
+++ capstone-backend/src/main/java/com/example/springbackend/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.springbackend.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // protect endpoint /api/orders
+        http.authorizeRequests()
+                .antMatchers("/api/orders/**")
+                // protect the endpoint that is only accessible to authenticated users
+                .authenticated()
+                .and()
+                // configure OAuth2 Resource Server support
+                .oauth2ResourceServer()
+                // enables JWT-encoded bearer token support
+                .jwt();
+
+        // add CORS filters
+        http.cors();
+
+        // force a non-empty response body for 401's to make the response more friendly
+        Okta.configureResourceServer401ResponseBody(http);
+
+        // disable CSRF since we are not using Cookies for session tracking
+        http.csrf().disable();
+    }
+
+}
