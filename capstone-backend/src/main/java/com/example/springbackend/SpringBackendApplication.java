3241386fddbbfa48a3872f1880c90fd20f139cc7
seanjungmsba <seanjeong1996@gmail.com>
2022-09-12 18:18:57 -0400

    spring runner updated

--- capstone-backend/src/main/java/com/example/springbackend/SpringBackendApplication.java
+++ capstone-backend/src/main/java/com/example/springbackend/SpringBackendApplication.java
@@ -2,11 +2,14 @@ package com.example.springbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableSwagger2
 public class SpringBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBackendApplication.class, args);
 	}
-
 }
