6f83af3f8fe63bffdb3e474f6717c4dafabeddf8
seanjungmsba <seanjeong1996@gmail.com>
2022-09-14 19:43:35 -0400

    order repository updated

--- spring-boot-backend/src/main/java/com/example/springbackend/repository/OrderRepository.java
+++ spring-boot-backend/src/main/java/com/example/springbackend/repository/OrderRepository.java
@@ -1,6 +1,5 @@
 package com.example.springbackend.repository;
 
-import com.example.springbackend.model.Customer;
 import com.example.springbackend.model.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
